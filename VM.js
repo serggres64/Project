c02298f6d60ac7fffd02c7d11a13493ab93dccc8
serggres64 <marjas554@gmail.com>
2024-05-02 00:04:01 +0300

    first

---
+++ VM.js
@@ -0,0 +1,181 @@
+const a = [
+    {
+        id : 1,
+        theory : "Матрица – прямоугольная таблица значений, где каждое значение имеет 2 индекса i, j, где i – номер строки j – номер столбца. Сложение матриц - это операция, при которой каждый элемент одной матрицы складывается с соответствующим элементом другой матрицы. Сложение выполняется только для матриц одинакового размера. Результат сложения - новая матрица, полученная путем сложения соответствующих элементов исходных матриц. Умножение матрицы на число - это операция, при которой каждый элемент матрицы умножается на это число.Умножение матрицы на число - это алгебраическая операция, которая умножает каждый элемент матрицы на заданное число. Матрицу A можно умножить на матрицу B только в том случае, если число столбцов матрицы A равняется числу строк матрицы B. Матрицы, для которых данное условие не выполняется, умножать нельзя.",
+        theory1 : "",
+        theoryPhoto: "VM-Task/Матрицы. Действия над матрицами - Theory.png",
+        taskPhoto: "VM-Task/Матрицы. Действия над матрицами - Task.png",
+        taskPhoto2: "",
+    },
+
+    
+    {
+        id : 2,
+        theory : "Определителем квадратной матрицы порядка n называется алгебраическая сумма n членов, каждый из которых равен произведению элементов, взятых по одному и только по одному из каждой строки и каждого столбца матрицы А.",
+        theory1 : "",
+        theoryPhoto: "VM-Task/Определитель матрицы - Theory.png",
+        taskPhoto: "VM-Task/Определитель матрицы - Theory1.png",
+        taskPhoto2: "VM-Task/Определитель матрицы - Task.png",
+    },
+    {
+        id : 3,
+        theory : "Метод Гаусса — классический метод решения системы линейных ал-гебраических уравнений (СЛАУ). Назван в честь немецкого математи-ка Карла Фридриха Гаусса. Это метод последовательного исключения переменных, когда с помощью элементарных преобразований система уравнений приводится к равносильной системе треугольного вида, из которой последовательно, начиная с последних (по номеру), находятся все переменные системы.",
+        theory1 : "Решение систем линейных уравнений методом Гаусса заключается в приведении ее с помощью элементарных преобразований к ступенча-тому виду. Ведущим коэффициентом уравнения называется первый отличный от нуля коэффициент.",
+        theoryPhoto: "VM-Task/Метод Гаусса - Theory.png",
+        taskPhoto: "VM-Task/Метод Гаусса - Task.png",
+        taskPhoto2: "VM-Task/Метод Гаусса - Task1.png",
+    },
+    {
+        id : 4,
+        theory : "Метод Крамера — способ решения систем линейных алгебраических уравнений (СЛАУ) с числом уравнений равным числу неизвестных с ненулевым главным определителем матрицы коэффициентов системы (причём для таких уравнений решение существует и единственно).",
+        theory1 : "",
+        theoryPhoto: "VM-Task/Метод Крамера - Theory.png",
+        taskPhoto: "VM-Task/Метод Крамера - Task.png",
+        taskPhoto2: "VM-Task/Метод Крамера - Task1.png",
+    },
+    {
+        id : 5,
+        theory : "",
+        theory1 : "",
+        theoryPhoto: "VM-Task/Кривые второго порядка на плоскости - Theory.png",
+        taskPhoto: "VM-Task/Кривые второго порядка на плоскости - Task.png",
+        taskPhoto2: "VM-Task/Кривые второго порядка на плоскости - Task1.png",
+        taskPhoto3: "VM-Task/Кривые второго порядка на плоскости - Task2.png",
+        taskPhoto4: "VM-Task/Кривые второго порядка на плоскости - Task3.png",
+        taskPhoto5: "VM-Task/Кривые второго порядка на плоскости - Task4.png",
+        taskPhoto6: "VM-Task/Кривые второго порядка на плоскости - Task5.png",
+        taskPhoto7: "VM-Task/Кривые второго порядка на плоскости - Task6.png",
+        taskPhoto8: "VM-Task/Кривые второго порядка на плоскости - Task7.png",
+    },
+    {
+        id : 6,
+        theory : "Алгебраической поверхностью второго порядка называется геометри-ческое место точек пространства, которое в какой-либо аффинной си-стеме координат Oxyz может быть задано уравнением вида.",
+        theory1 : "",
+        theoryPhoto: "VM-Task/Поверхности второго порядка - Theory.png",
+        taskPhoto: "VM-Task/Поверхности второго порядка - Task.png",
+        taskPhoto2: "",
+    },
+    {
+        id : 7,
+        theory : "Комплексное число - это число вида a + bi",
+        theory1 : "",
+        theoryPhoto: "VM-Task/Комплексные числа - Theory.png",
+        taskPhoto: "VM-Task/Комплексные числа - Task.png",
+        taskPhoto2: "VM-Task/Комплексные числа - Task1.png",
+        taskPhoto3: "VM-Task/Комплексные числа - Task2.png",
+        taskPhoto4: "VM-Task/Комплексные числа - Task3.png",
+        taskPhoto5: "VM-Task/Комплексные числа - Task4.png",
+        taskPhoto6: "",
+        taskPhoto7: "",
+        taskPhoto8: "",
+    },
+    {
+        id : 8,
+        theory : "",
+        theory1 : "",
+        theoryPhoto: "VM-Task/Пределы - Theory.png",
+        taskPhoto: "VM-Task/Пределы - Task.png",
+        taskPhoto2: "VM-Task/Пределы - Task1.png",
+        taskPhoto3: "VM-Task/Пределы - Task2.png",
+        taskPhoto4: "VM-Task/Пределы - Task3.png",
+        taskPhoto5: "VM-Task/Пределы - Task4.png",
+        taskPhoto6: "",
+        taskPhoto7: "",
+        taskPhoto8: "",
+    },
+    {
+        id : 9,
+        theory : "Правила нахождения производной функции многих переменных (нахождение частных производных): частной производной по какой-либо переменной называется производная по этой переменной все остальные переменные считаются постоянной величиной",
+        theory1 : "",
+        theoryPhoto: "VM-Task/Функция многих переменных - Theory.png",
+        taskPhoto: "VM-Task/Функция многих переменных - Task.png",
+        taskPhoto2: "",
+        taskPhoto3: "",
+        taskPhoto4: "",
+        taskPhoto5: "",
+        taskPhoto6: "",
+        taskPhoto7: "",
+        taskPhoto8: "",
+    },
+    {
+        id : 10,
+        theory : "",
+        theory1 : "",
+        theoryPhoto: "VM-Task/Вычисление производных сложных функций - Theory.png",
+        taskPhoto: "VM-Task/Вычисление производных сложных функций - Task.png",
+        taskPhoto2: "VM-Task/Вычисление производных сложных функций - Task1.png",
+    },
+    {
+        id : 11,
+        theory : "",
+        theory1 : "",
+        theoryPhoto: "VM-Task/Интегральное исчисление - Theory.png",
+        taskPhoto: "VM-Task/Интегральное исчисление - Task.png",
+        taskPhoto2: "VM-Task/Интегральное исчисление - Task1.png",
+        taskPhoto3: "VM-Task/Интегральное исчисление - Task2.png",
+        taskPhoto4: "",
+        taskPhoto5: "",
+        taskPhoto6: "",
+        taskPhoto7: "",
+        taskPhoto8: "",
+    },
+    {
+        id : 12,
+        theory : "",
+        theory1 : "",
+        theoryPhoto: "VM-Task/Дифференциальные уравнения первого порядка - Theory.png",
+        taskPhoto: "VM-Task/Дифференциальные уравнения первого порядка - Task.png",
+        taskPhoto2: "VM-Task/Дифференциальные уравнения первого порядка - Task1.png",
+        taskPhoto3: "VM-Task/Дифференциальные уравнения первого порядка - Task2.png",
+        taskPhoto4: "VM-Task/Дифференциальные уравнения первого порядка - Task3.png",
+        taskPhoto5: "",
+        taskPhoto6: "",
+        taskPhoto7: "",
+        taskPhoto8: "",
+    }
+];
+
+const btn = document.querySelector('.btn-main');
+const select = document.querySelector('.page-number');
+const users = document.querySelector('.users');
+
+
+users.classList.add('user');
+users.innerHTML = `
+    <p>${a[0].theory}</p>
+    <p>${a[0].theory1}</p>
+    <img src="${a[0].theoryPhoto}" alt="">
+    <img src="${a[0].taskPhoto}" alt="">
+    <img src="${a[0].taskPhoto2}" alt="">
+`;
+
+
+select.onchange = function() {
+    let item = select.value;
+    console.log(item);
+    
+
+for (let elem of a) {
+    
+    if (elem.id == Number(item)) {
+        console.log(a[item - 1]);
+        users.innerHTML = "";
+        const element = document.createElement('div');
+        element.classList.add('user');
+        element.innerHTML = `
+            <p>${elem.theory}</p>
+            <p>${elem.theory1}</p>
+            <img src="${elem.theoryPhoto}" alt="">
+            <img src="${elem.taskPhoto}" alt="">
+            <img src="${elem.taskPhoto2}" alt="">
+            <img src="${elem.taskPhoto3}" alt="">
+            <img src="${elem.taskPhoto4}" alt="">
+            <img src="${elem.taskPhoto5}" alt="">
+            <img src="${elem.taskPhoto6}" alt="">
+            <img src="${elem.taskPhoto7}" alt="">
+            <img src="${elem.taskPhoto8}" alt="">
+        `;
+        users.appendChild(element);
+    }
+}
+};
